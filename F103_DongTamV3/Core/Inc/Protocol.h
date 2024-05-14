80431595ce878bc10a335593ce2f3458664cb076
TM-khoa <minhkhoa9a31314@gmail.com>
2024-05-14 15:26:19 +0700

    change commandlist to protocolListID, make module more abstract by using portCommunicate

--- F103_DongTamV3/Core/Inc/Protocol.h
+++ F103_DongTamV3/Core/Inc/Protocol.h
@@ -21,17 +21,19 @@ extern "C" {
 }
 #endif
 
-typedef enum CommandList {
-	CMD_HANDSHAKE,
-	CMD_VALVE,
-	CMD_PULSE_TIME,
-	CMD_TOTAL_VALVE,
-	CMD_CYCLE_INTERVAL_TIME,
-	CMD_INTERVAL_TIME,
-	CMD_TRIGGER_VALVE,
-	CMD_RTC_TIME,
-	CMD_END,
-} CommandList;
+typedef enum ProtocolListID {
+	PROTOCOL_ID_HANDSHAKE,
+	PROTOCOL_ID_VALVE,
+	PROTOCOL_ID_PULSE_TIME,
+	PROTOCOL_ID_TOTAL_VALVE,
+	PROTOCOL_ID_CYCLE_INTERVAL_TIME,
+	PROTOCOL_ID_INTERVAL_TIME,
+	PROTOCOL_ID_TRIGGER_VALVE,
+	PROTOCOL_ID_RTC_TIME,
+	PROTOCOL_ID_PRESSURE,
+	PROTOCOL_ID_ERROR,
+	PROTOCOL_ID_END,
+} ProtocolListID;
 
 typedef enum GetSetFlag {
 	SET_DATA_TO_THIS_DEVICE,
@@ -48,8 +50,10 @@ typedef enum ProtocolErrorCode {
 	PROTOCOL_ERR_CRC_FAIL,
 	PROTOCOL_ERR_FRAME_ERROR,
 	PROTOCOL_ERR_OUT_OF_BUFFER_SIZE,
-	PROTOCOL_ERR_UART_PORT_NULL,
+	PROTOCOL_ERR_PORT_NULL,
 	PROTOCOL_ERR_SEND_FRAME_FAIL,
+	PROTOCOL_ERR_MISSING_SEND_FUNCTION,
+	PROTOCOL_ERR_MISSING_RECEIVE_FUNCTION,
 	PROTOCOL_ERR_REFERENCE_PAYLOAD_NOT_FOUND,
 	PROTOCOL_ERR_PAYLOAD_NOT_RECOGNIZE,
 	PROTOCOL_ERR_STORE_BUFFER_IS_NULL,
@@ -62,13 +66,13 @@ typedef struct FrameData {
 		uint16_t crc16;
 		// avoid critical section by sending and receiving frame in the same FrameData variable
 		bool isOnProcess;
-		CommandList cmdList;
+		ProtocolListID protocolID;
 } FrameData;
 
-typedef struct ArgumentOfCommandList_t {
+typedef struct ArgumentOfProtocolList_t {
 		void *pArg;
 		uint8_t sizeArgument;
-} ArgumentOfCommandList_t;
+} ArgumentOfProtocolList_t;
 
 #define PROTOCOL_COMMAND_LIST_LENGTH 1
 #define PROTOCOL_CRC_LENGTH	2
@@ -77,20 +81,20 @@ typedef struct ArgumentOfCommandList_t {
 #define PROTOCOL_TOTAL_LENGTH(_DATASIZE_) (PROTOCOL_COMMAND_LIST_LENGTH + PROTOCOL_DATA_LENGTH + PROTOCOL_GET_SET_LENGTH + (_DATASIZE_) + PROTOCOL_CRC_LENGTH)
 
 #define PROTOCOL_DATA_LENGTH_FIELD 0
-#define PROTOCOL_COMMANDLIST_FIELD (PROTOCOL_DATA_LENGTH_FIELD + PROTOCOL_DATA_LENGTH)
-#define PROTOCOL_GET_SET_FIELD (PROTOCOL_COMMANDLIST_FIELD + PROTOCOL_COMMAND_LIST_LENGTH)
+#define PROTOCOL_ID_FIELD (PROTOCOL_DATA_LENGTH_FIELD + PROTOCOL_DATA_LENGTH)
+#define PROTOCOL_GET_SET_FIELD (PROTOCOL_ID_FIELD + PROTOCOL_COMMAND_LIST_LENGTH)
 #define PROTOCOL_PAYLOAD_FIELD (PROTOCOL_GET_SET_FIELD + PROTOCOL_GET_SET_LENGTH)
 #define PROTOCOL_CRC16_FIELD(_DATASIZE_) (PROTOCOL_PAYLOAD_FIELD + (_DATASIZE_))
 
-typedef void (*pProtocolCpltCallback)(CommandList cmdlist);
+typedef void (*pProtocolCpltCallback)(ProtocolListID protocolID, GetSetFlag getOrSet);
 typedef void (*pProtocolErrorCallback)(ProtocolErrorCode err);
-typedef void (*pProtocolSend)(uint8_t *pData, uint16_t Size);
-typedef void (*pProtocolReceiveInterrupt)(uint8_t *pData, uint16_t Size);
+typedef void (*pProtocolSend)(void *portCommunicate, uint8_t *pData, uint16_t Size);
+typedef void (*pProtocolReceiveInterrupt)(void *portCommunicate, uint8_t *pData, uint16_t Size);
 
 class Protocol {
 
 	private:
-		UART_HandleTypeDef *_port;
+		void *_port;
 		uint8_t *_pTxBuffer;
 		uint8_t *_pRxBuffer;
 		uint8_t _rxBufSize;
@@ -102,27 +106,27 @@ class Protocol {
 		bool isRxBufferEmpty = true;
 		bool isTxBufferEmpty = true;
 		FrameData fd;
-		ArgumentOfCommandList_t argcmd[CMD_END - 1];
+		ArgumentOfProtocolList_t argID[PROTOCOL_ID_END - 1];
 
-		ProtocolErrorCode decodeFrameAndCheckCRC();
-		void jumpToError(ProtocolErrorCode err);
-		void resetFrame();
-		bool isPassCRC();
+		ProtocolErrorCode DecodeFrameAndCheckCRC();
+		void JumpToError(ProtocolErrorCode err);
+		void ResetFrame();
+		bool IsPassCRC();
 
 	public:
 
-		void init(UART_HandleTypeDef *huart, uint8_t *pTxBuffer, uint8_t txSize, uint8_t *pRxBuffer, uint8_t rxSize);
-		void registerSendFunction(uint8_t *pData, uint16_t Size);
-		void registerReceiveFunction(uint8_t *pData, uint16_t Size);
-		void registerArgument(void *arg, uint8_t sizeOfArgument, CommandList cmdID);
-		void registerReceivedCallbackEvent(void (*pComCpltCallback)(CommandList cmdlist));
-		void registerErrorEvent(void (*pComErrorCallback)(ProtocolErrorCode err));
-		void getValueFromPayload();
-		void getValueFromPayload(void *outData, uint8_t sizeOfData);
-		void receiveDataInterrupt(UART_HandleTypeDef *huart);
-		void makeFrame(CommandList cmdID, GetSetFlag getOrSet);
-		void makeFrame(void *payload, uint16_t sizeOfPayload, CommandList cmdID, GetSetFlag getOrSet);
-		void sendFrame();
+		void Init(void *portCommunicate, uint8_t *pTxBuffer, uint8_t txSize, uint8_t *pRxBuffer, uint8_t rxSize);
+		void RegisterArgument(void *arg, uint8_t sizeOfArgument, ProtocolListID protocolID);
+		void RegisterReceivedCallbackEvent(void (*pComCpltCallback)(ProtocolListID protocolID, GetSetFlag getOrSet));
+		void RegisterErrorEvent(void (*pComErrorCallback)(ProtocolErrorCode err));
+		void RegisterSendFrameFunction(void (*pProtocolSend)(void *portCommunicate, uint8_t *pData, uint16_t Size));
+		void RegisterReceiveFrameFunction(void (*pProtocolReceiveInterrupt)(void *portCommunicate, uint8_t *pData, uint16_t Size));
+		void GetValueFromPayload();
+		void GetValueFromPayload(void *outData, uint8_t sizeOfData);
+		void HandleReceiveDataInterrupt(void *portCommunicate);
+		void MakeFrame(ProtocolListID protocolID, GetSetFlag getOrSet);
+		void MakeFrame(void *payload, uint16_t sizeOfPayload, ProtocolListID protocolID, GetSetFlag getOrSet);
+		void SendFrame();
 
 };
 
