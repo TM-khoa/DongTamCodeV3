bbd8d6505e300ec53bdd7a153f83dac0b1223324
TM-khoa <minhkhoa9a31314@gmail.com>
2024-05-27 02:13:53 +0700

    feat: Handle Send RTC and Pressure AMS5915 and SP100

--- F103_DongTamV3/Core/Inc/MessageHandle.h
+++ F103_DongTamV3/Core/Inc/MessageHandle.h
@@ -10,16 +10,95 @@
 
 #include "main.h"
 #include "Protocol.h"
+#include "RTC_Format.h"
 
-class MessageHandle: Protocol {
-	public:
-		void HandleCommandID(ProtocolListID protocolID, GetSetFlag getOrSet);
-		void CallbackFromUART(UART_HandleTypeDef *huart);
-		MessageHandle();
-		virtual ~MessageHandle();
+typedef HAL_StatusTypeDef (*pfnSend)(UART_HandleTypeDef *huart, const uint8_t *pData, uint16_t Size, uint32_t Timeout);
+typedef HAL_StatusTypeDef (*pfnReceive)(UART_HandleTypeDef *huart, uint8_t *pData, uint16_t Size);
+typedef struct PressureTwoSensorValue {
+		float sensorAMS5915;
+		float sensorSP100;
+} PressureTwoSensorValue;
 
+void HandleMessageCallback(uint8_t *inputBuffer, uint16_t sizeOfInputBuffer, ProtocolListID protocolID, GetSetFlag getOrSet);
+
+class MessageHandle: public Protocol {
 	private:
 		bool _isHandShake;
-};
+		UART_HandleTypeDef *_targetUART;
+		UART_HandleTypeDef *portArray[2];
+		uint8_t _rxBuf[50];
+		uint8_t _txBuf[50];
+		pfnSend pSend;
+		pfnReceive pReceive;
+		uint16_t _handshakeCode;
+		PressureTwoSensorValue _pressure;
+	public:
+		MessageHandle() {
+			pSend = &HAL_UART_Transmit;
+			pReceive = &HAL_UART_Receive_IT;
+			Protocol::RegisterStorageBuffer(_txBuf, sizeof(_txBuf), _rxBuf, sizeof(_rxBuf));
+			Protocol::RegisterReceivedCallbackEvent(&HandleMessageCallback);
+
+		}
+
+		void HandleFramDataInterrupt(UART_HandleTypeDef *huart) {
+			_targetUART = huart;
+			static bool isOnFrameReceived = false;
+			if (!isOnFrameReceived) {
+				isOnFrameReceived = true;
+				uint8_t totalLength = Protocol::GetTotalLengthFromFirstCallback();
+				if (totalLength < sizeof(_rxBuf)) {
+					pReceive(_targetUART, (uint8_t*) _rxBuf + PROTOCOL_TOTAL_LENGTH_FIELD_SIZE, totalLength - PROTOCOL_TOTAL_LENGTH_FIELD_SIZE);
+				}
+				else
+					JumpToError(PROTOCOL_ERR_OUT_OF_BUFFER_SIZE);
+			}
+			else {
+				Protocol::DecodeFrameAndCheckCRC();
+				isOnFrameReceived = false;
+				FrameData fd = GetFrameDataInfo();
+				memset(_rxBuf, 0, fd.totalLength);
+				Protocol::ResetFrame();
+			}
+		}
+		void Begin(UART_HandleTypeDef *huartESP32, UART_HandleTypeDef *huartLog) {
+			portArray[0] = huartESP32;
+			portArray[1] = huartLog;
+			_targetUART = portArray[0];
+			pReceive(*(portArray + 0), _rxBuf, 1);
+			pReceive(*(portArray + 1), _rxBuf, 1);
+
+			Protocol::RegisterArgument((void*) &_pressure, sizeof(PressureTwoSensorValue), PROTOCOL_ID_PRESSURE);
+			Protocol::RegisterArgument((void*) &_handshakeCode, 0, PROTOCOL_ID_HANDSHAKE);
+			SendFrame(PROTOCOL_ID_HANDSHAKE, GET_DATA_FROM_THIS_DEVICE);
+
+		}
+		void SetPortUART(UART_HandleTypeDef *huart) {
+			_targetUART = huart;
+		}
+
+		uint8_t* GetPtrRxBuffer() {
+			return _rxBuf;
+		}
+		uint8_t* GetPtrTxBuffer() {
+			return _rxBuf;
+		}
+
+		void UpdatePressureAndSend(UART_HandleTypeDef *huart, float pAMS5915, float pSP100) {
+			_pressure.sensorAMS5915 = pAMS5915;
+			_pressure.sensorSP100 = pSP100;
+			SendFrame(PROTOCOL_ID_PRESSURE, SET_DATA_TO_THIS_DEVICE);
+		}
+
+		void SendFrame(ProtocolListID protocolID, GetSetFlag getSetFlag) {
+			Protocol::MakeFrame(protocolID, getSetFlag);
+			FrameData fd = GetFrameDataInfo();
+			pSend(_targetUART, _txBuf, fd.totalLength, HAL_MAX_DELAY);
+			Protocol::ResetFrame();
+			memset(_txBuf, 0, fd.totalLength);
+		}
+
+}
+;
 
 #endif /* INC_MESSAGEHANDLE_H_ */
--- F103_DongTamV3/Core/Src/MessageHandle.cpp
+++ F103_DongTamV3/Core/Src/MessageHandle.cpp
@@ -9,54 +9,13 @@
 
 extern UART_HandleTypeDef huart1;
 extern UART_HandleTypeDef huart3;
+MessageHandle mesg;
 
-uint8_t logTxBuf[70] = {0};
-uint8_t logRxBuf[70] = {0};
-uint8_t espTxBuf[70] = {0};
-uint8_t espRxBuf[70] = {0};
+void HandleMessageCallback(uint8_t *inputBuffer, uint16_t sizeOfInputBuffer, ProtocolListID protocolID, GetSetFlag getOrSet) {
 
-Protocol pLog, pESP32;
-
-void MessageSend(void *portCommunicate, uint8_t *data, uint16_t size) {
-	HAL_UART_Transmit((UART_HandleTypeDef*) portCommunicate, data, size, HAL_MAX_DELAY);
-}
-
-void MessageReceive(void *portCommunicate, uint8_t *data, uint16_t size) {
-	HAL_UART_Receive_IT((UART_HandleTypeDef*) portCommunicate, data, size);
-}
-
-void MessageHandleCommandID(ProtocolListID protocolID, GetSetFlag getOrSet) {
-
-}
-
-void MessageHandle::CallbackFromUART(UART_HandleTypeDef *huart) {
-	pLog.HandleReceiveDataInterrupt((void*) &huart);
-	pESP32.HandleReceiveDataInterrupt((void*) &huart);
 }
 
 void MessageError(ProtocolErrorCode err) {
 	while (1);
-}
-
-MessageHandle::MessageHandle() {
-	// TODO Auto-generated constructor stub
-
-	pLog.RegisterReceiveFrameFunction(&MessageReceive);
-	pLog.RegisterSendFrameFunction(&MessageSend);
-	pLog.RegisterErrorEvent(&MessageError);
-	pLog.RegisterReceivedCallbackEvent(&MessageHandleCommandID);
-	pLog.Init((void*) &huart3, logTxBuf, sizeof(logTxBuf), logRxBuf, sizeof(logRxBuf));
-
-	pESP32.RegisterReceiveFrameFunction(&MessageReceive);
-	pESP32.RegisterSendFrameFunction(&MessageSend);
-	pESP32.RegisterErrorEvent(&MessageError);
-	pESP32.RegisterReceivedCallbackEvent(&MessageHandleCommandID);
-	pESP32.Init((void*) &huart1, espTxBuf, sizeof(espTxBuf), espRxBuf, sizeof(espRxBuf));
-
-	_isHandShake = false;
-}
-
-MessageHandle::~MessageHandle() {
-	// TODO Auto-generated destructor stub
 }
 
