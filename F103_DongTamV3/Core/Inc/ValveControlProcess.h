b436f08b4e6ebc03d13f30e3200762bc45ec1156
TM-khoa <minhkhoa9a31314@gmail.com>
2024-05-29 13:03:09 +0700

    Move part of methods implement code from .cpp to .h of ValveControlProcess

--- F103_DongTamV3/Core/Inc/ValveControlProcess.h
+++ F103_DongTamV3/Core/Inc/ValveControlProcess.h
@@ -34,10 +34,15 @@ typedef enum ValveControlProcessStep {
 
 class ValveFeedback {
 	public:
-		ValveFeedback();
-		~ValveFeedback();
-		void SetInputValue(uint16_t value);
-		uint16_t GetInputValue();
+		ValveFeedback() {
+			HC165_AssignPin(&expanderInput, _74HC165_CLK_GPIO_Port, _74HC165_CLK_Pin, HC165_CP);
+			HC165_AssignPin(&expanderInput, _74HC165_DATA_GPIO_Port, _74HC165_DATA_Pin, HC165_DATA);
+			HC165_AssignPin(&expanderInput, _74HC165_LOAD_GPIO_Port, _74HC165_LOAD_Pin, HC165_PL);
+		}
+		uint16_t GetInputValue() {
+			inputValue = (uint16_t) HC165_ReadState(EXPANDER_INPUT_PORT);
+			return inputValue;
+		}
 	private:
 		uint32_t inputValue;
 		HC165_t expanderInput;
@@ -63,27 +68,102 @@ class ValveControl {
 		void NextValve();
 
 	public:
-		ValveControl();
-		~ValveControl();
-		void AssignControlPin(GPIO_TypeDef *gpio, uint16_t pin, HC595_PinName HC595_PinName);
-		void IncreaseTick();
-		void SetTick(uint16_t tick);
-		uint16_t GetTick();
+		ValveControl() {
+			AssignControlPin(_74HC595_CLK_GPIO_Port, _74HC595_CLK_Pin, HC595_CLK);
+			AssignControlPin(_74HC595_DATA_GPIO_Port, _74HC595_DATA_Pin, HC595_DS);
+			AssignControlPin(_74HC595_STORE_GPIO_Port, _74HC595_STORE_Pin, HC595_LATCH);
+			_totalValve = 16;
+			_pulseTime = 50;
+			_intervalTime = 1;
+			_cycleIntervalTime = 3;
 
-		void SetTotalValve(uint8_t totalValve);
-		uint8_t GetTotalValve();
+			if (_totalValve > MAX_NUM_VAN)
+				while (1);
+			uint8_t n = 0;
+			if (_totalValve > 8)
+				n = 2;
+			else if (_totalValve < 8 && _totalValve > 0)
+				n = 1;
+			HC595_ClearByteOutput(0xffffffff);
+			HC595_ShiftOut(NULL, n, 1);
+			_isOnProcess = false;
+		}
 
-		void SetPulseTime(uint16_t pulseTime);
-		uint16_t GetPulseTime();
+		void AssignControlPin(GPIO_TypeDef *gpio, uint16_t pin, HC595_PinName HC595_PinName) {
+			if (HC595_AssignPin(&_hc595, gpio, pin, HC595_PinName) != HC595_OK)
+				while (1);
+		}
 
-		void SetIntervalTime(uint16_t intervalTime);
-		uint16_t GetIntervalTime();
+		void IncreaseTick() {
+			_timerTick++;
+		}
+		void SetTick(uint16_t tick) {
+			_timerTick = tick;
+		}
+		uint16_t GetTick() {
+			return _timerTick;
+		}
 
-		void SetCycleIntervalTime(uint8_t cycleIntervalTime);
-		uint8_t GetCycleIntervalTime();
+		void SetTotalValve(uint8_t totalValve) {
+			if (totalValve > MAX_NUM_VAN)
+				_totalValve = MAX_NUM_VAN;
+			else
+				_totalValve = totalValve;
+			if (_processStep != PROCESS_IDLE)
+				_processStep = PROCESS_START;
+		}
+		uint8_t GetTotalValve() {
+			return _totalValve;
+		}
 
-		void SetOutputValve(uint8_t valve, bool on);
-		void SetOutputMultiValve(uint16_t valve, bool on);
+		void SetPulseTime(uint16_t pulseTime) {
+			if (pulseTime < 30)
+				pulseTime = 30;
+			else if (pulseTime > 300)
+				pulseTime = 300;
+			else
+				_pulseTime = pulseTime;
+		}
+		uint16_t GetPulseTime() {
+			return _pulseTime;
+		}
+
+		void SetIntervalTime(uint16_t intervalTime) {
+			if (intervalTime < 0)
+				intervalTime = 0;
+			else if (intervalTime > 100)
+				_intervalTime = 100;
+			else
+				_intervalTime = intervalTime;
+		}
+		uint16_t GetIntervalTime() {
+			return _intervalTime;
+		}
+
+		void SetCycleIntervalTime(uint8_t cycleIntervalTime) {
+			if (cycleIntervalTime < 2)
+				_cycleIntervalTime = 2;
+			else if (cycleIntervalTime > 100)
+				_cycleIntervalTime = 100;
+			else
+				_cycleIntervalTime = cycleIntervalTime;
+
+		}
+		uint8_t GetCycleIntervalTime() {
+			return _cycleIntervalTime;
+		}
+
+		void SetOutputValve(uint8_t valve, bool on) {
+			if (valve > MAX_NUM_VAN)
+				while (1);
+			if (on) {
+				HC595_SetBitOutput(valve);
+			}
+			else {
+				HC595_ClearBitOutput(valve);
+			}
+			HC595_ShiftOut(NULL, 2, 1);
+		}
 
 		bool IsOnProcess();
 		ValveControlProcessStep GetProcessStep();
