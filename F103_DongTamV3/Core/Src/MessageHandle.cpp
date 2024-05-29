9692400760ff60f1a813b9915243b2c406b6f62e
TM-khoa <minhkhoa9a31314@gmail.com>
2024-05-29 13:01:07 +0700

    feat: Send handshake command to ESP32 when request

--- F103_DongTamV3/Core/Src/MessageHandle.cpp
+++ F103_DongTamV3/Core/Src/MessageHandle.cpp
@@ -11,8 +11,11 @@ extern UART_HandleTypeDef huart1;
 extern UART_HandleTypeDef huart3;
 MessageHandle mesg;
 
-void HandleMessageCallback(uint8_t *inputBuffer, uint16_t sizeOfInputBuffer, ProtocolListID protocolID, GetSetFlag getOrSet) {
-
+void HandleMessageCallback(uint8_t *inputBuffer, uint16_t sizeOfInputBuffer, ProtocolListID id, GetSetFlag getSetFlag) {
+	if (id == PROTOCOL_ID_HANDSHAKE && getSetFlag == GET_DATA_FROM_THIS_DEVICE) {
+		mesg.SetHandshakeStatus(true);
+		mesg.SendFrame(PROTOCOL_ID_HANDSHAKE, SET_DATA_TO_THIS_DEVICE);
+	}
 }
 
 void MessageError(ProtocolErrorCode err) {
