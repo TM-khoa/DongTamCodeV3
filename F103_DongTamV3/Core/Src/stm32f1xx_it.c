c659f070a005b418fe95e1820b2ccdfc5e355a59
TM-khoa <minhkhoa9a31314@gmail.com>
2024-05-29 12:56:16 +0700

    feat: Add IWDG in stm32 to prevent stuck when plug-in 220V~

--- F103_DongTamV3/Core/Src/stm32f1xx_it.c
+++ F103_DongTamV3/Core/Src/stm32f1xx_it.c
@@ -57,7 +57,6 @@
 /* External variables --------------------------------------------------------*/
 extern TIM_HandleTypeDef htim3;
 extern UART_HandleTypeDef huart1;
-extern UART_HandleTypeDef huart3;
 /* USER CODE BEGIN EV */
 
 /* USER CODE END EV */
@@ -226,20 +225,6 @@ void USART1_IRQHandler(void)
   /* USER CODE BEGIN USART1_IRQn 1 */
 
   /* USER CODE END USART1_IRQn 1 */
-}
-
-/**
-  * @brief This function handles USART3 global interrupt.
-  */
-void USART3_IRQHandler(void)
-{
-  /* USER CODE BEGIN USART3_IRQn 0 */
-
-  /* USER CODE END USART3_IRQn 0 */
-  HAL_UART_IRQHandler(&huart3);
-  /* USER CODE BEGIN USART3_IRQn 1 */
-
-  /* USER CODE END USART3_IRQn 1 */
 }
 
 /**
